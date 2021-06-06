865b548a1f5660f8970f8ac231e0cb090eeb5e4e
Łukasz Łakomy <l.lakomy@webcon.pl>
2021-06-06 02:05:03 +0200

    add migrations

---
+++ Migrations/20210606000053_ChangePrescriptionModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RemindONServer.Migrations
+{
+    public partial class ChangePrescriptionModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
