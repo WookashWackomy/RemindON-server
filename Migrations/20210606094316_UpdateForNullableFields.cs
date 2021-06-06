5694cffe3193de9cf1fe2306b210212bef48526d
Łukasz Łakomy <l.lakomy@webcon.pl>
2021-06-06 11:44:26 +0200

    remove unused views

---
+++ Migrations/20210606094316_UpdateForNullableFields.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RemindONServer.Migrations
+{
+    public partial class UpdateForNullableFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "RemindONDevices",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "RemindONDevices");
+        }
+    }
+}
